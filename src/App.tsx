2b5810bd9cdd510536ff8c146cc1bee6d9eb7ccc
RubenOlander <rubenolander@gmail.com>
2023-05-24 08:59:52 +0200

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import './App.css';
 import mapboxgl from 'mapbox-gl';
 import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    initilizeISS();
+    initializeISS();
   }, []);
 
   mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
@@ -18,7 +17,7 @@ function App() {
     });
   }
 
-  async function initilizeISS(): Promise<void> {
+  async function initializeISS() {
     const response = await fetch("http://api.open-notify.org/iss-now.json");
     const iss = await response.json();
     const longitude = iss.iss_position.longitude;
@@ -28,9 +27,9 @@ function App() {
 
   return (
     <>
-    <div id="flex">
-      <img className="fixed w-80 z-10 pointer-events-none" id="ISS" src="../src/assets/iss.png" />
-      <div id="map"></div>
+    <div className="flex justify-center items-center">
+      <img className="w-80 z-10 pointer-events-none fixed" src="../src/assets/iss.png" />
+      <div className="w-[100vw] h-[100vh] relative" id="map"></div>
     </div>
     </>
   )
