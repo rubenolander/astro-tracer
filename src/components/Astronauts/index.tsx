873afa9c1d415e15a185e7bb88fc8c2be004c6c7
tobias-ahlund <tobias.ahlund@gmail.com>
2023-05-28 00:14:08 +0200

    made the menu clickable

--- src/components/Astronauts/index.tsx
+++ src/components/Astronauts/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import ArrowUp from "../../assets/arrow-up.svg";
+import ArrowDown from "../../assets/arrow-down.svg"
 import CrewIcon from "../../assets/crew-icon.svg"
 
 type Astronaut = {
@@ -8,6 +9,8 @@ type Astronaut = {
 };
 
 const Astronauts = () => {
+    const [showCrew, setShowCrew] = useState(false);
+
     useEffect(() => {
         getAstronauts();
     }, []);  
@@ -29,18 +32,21 @@ const Astronauts = () => {
 
     return (
         <section className="shadow-[inset_1px_0.1px_2px_rgba(250,250,250,0.5)] flex flex-col z-10 text-white bg-[color:var(--menu-blue)] rounded-tr-xl">
-            <div className="cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-select-blue)] border-l-8 border-l-solid">
+            <div 
+                onClick={() => setShowCrew(!showCrew ? true : false)} 
+                className={showCrew ? "cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-select-blue)] border-l-8 border-l-solid" : "cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-divider-blue)] border-l-8 border-l-solid"}
+                >
                 <div className="flex">
                     <img className="w-5 mr-2" src={CrewIcon} alt="crew icon" />
                     <p className="font-bold">Current ISS crew</p>
                 </div>
-                <img className="w-5 ml-4" src={ArrowUp} alt="collapse menu" />
+                <img className="w-5 ml-4" src={showCrew ? ArrowUp : ArrowDown} alt="collapse menu" />
             </div>
-            <ul className="border-b-[color:var(--menu-divider-blue)] border-b border-b-solid p-4 flex flex-col gap-1 text-sm bg-[color:var(--menu-expanded-blue)]">
+            {showCrew && <ul className="border-b-[color:var(--menu-divider-blue)] border-b border-b-solid p-4 flex flex-col gap-1 text-sm bg-[color:var(--menu-expanded-blue)]">
                 {issCrewMember.map((crewMember: string, index: number) => (
                     <li className="font-medium text-sm" key={index}>{crewMember}</li>
                     ))}
-            </ul>
+            </ul>}
         </section>
     );  
     
--- src/components/IssPositionCard/index.tsx
+++ src/components/IssPositionCard/index.tsx
@@ -1,5 +1,7 @@
 import ArrowUp from "../../assets/arrow-up.svg"
+import ArrowDown from "../../assets/arrow-down.svg"
 import PositionIcon from "../../assets/position-icon.svg"
+import { useState } from "react";
 
 type Position = {
     latitude: number|undefined,
@@ -7,20 +9,24 @@ type Position = {
 }
 
 const IssPositionCard = (props: Position) => {
+    const [showPosition, setShowPosition] = useState(false);
     return (
-        <section className="flex flex-col z-10 text-white bg-[color:var(--menu-blue)] rounded-br-xl">
-            <div className="cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-select-blue)] border-l-8 border-l-solid">
+        <section 
+            className={showPosition ? "flex flex-col z-10 text-white bg-[color:var(--menu-blue)] rounded-br-xl" : "flex flex-col z-10 text-white bg-[color:var(--menu-blue)]"}>
+            <div 
+                onClick={() => setShowPosition(!showPosition ? true : false)} className={showPosition ? "cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-select-blue)] border-l-8 border-l-solid" : "cursor-pointer p-4 pl-2 flex justify-between border-b-[color:var(--menu-divider-blue)] border-b border-b-solid border-l-[color:var(--menu-divider-blue)] border-l-8 border-l-solid"}
+                >
                 <div className="flex">
                     <img className="w-5 mr-2" src={PositionIcon} alt="position icon" />
                     <p className="font-bold">Current ISS location</p>
                 </div>
-                <img className="w-5 ml-4" src={ArrowUp} alt="collapse menu" />
+                <img className="w-5 ml-4" src={showPosition ? ArrowUp : ArrowDown} alt="collapse menu" />
             </div>
 
-            <div className="rounded-br-xl p-4 flex flex-col gap-1 text-sm bg-[color:var(--menu-expanded-blue)]">
+            {showPosition && <div className="rounded-br-xl p-4 flex flex-col gap-1 text-sm bg-[color:var(--menu-expanded-blue)]">
                 <p>Longitude: {props.longitude}</p>
                 <p>Latitude: {props.latitude}</p>
-            </div>
+            </div>}
         </section>
     )
 }
